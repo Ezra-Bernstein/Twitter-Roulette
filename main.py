e18944ce2ccd29a12db067b2edb53143899f2bbc
Ezra Bernstein <mremdb@gmail.com>
2021-07-16 12:02:30 -0400

    Updated main.py and html pages
    Basic framework for the game with form submissions and back-end support for getting the data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, session
 
 
 
@@ -9,6 +9,28 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-    
+@app.route("/joinGame", methods=['POST'])
+def joinGame():
 
+    code = request.form['code']
+    print(code)
+
+    # if (gameExists(code)):
+    return render_template('name.html')
+
+@app.route("/name", methods=['POST'])
+def name():
+
+    username = request.form['username']
+
+    #addUsertoGame(username)
+    return render_template('game.html')
+
+    
+@app.route("/guess", methods=['POST'])
+def guess():
+
+    guess = request.form['guess']
+    
+    return render_template('game.html')
 
