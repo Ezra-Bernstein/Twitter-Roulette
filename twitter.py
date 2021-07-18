566fd0d850d01cf39f0dacc883415b2edcf8b8c6
Ezra Bernstein <mremdb@gmail.com>
2021-07-18 00:23:33 -0400

    Update twitter.py
    Added random tweet function

--- twitter.py
+++ twitter.py
@@ -7,13 +7,9 @@ load_dotenv()
 
 BEARER=os.getenv('BEARER_TOKEN')
 
-r = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=jack&exclude_replies=true&include_rts=false&tweet_mode=extended&since_id=1&count=3200", headers={'Authorization': 'Bearer '+BEARER})
 
-#print(r.json())
-print(len(r.json()))
-
-# for tweet in r.json():
-#     print(tweet['full_text'] + "\n")
-
-index = random.randint(0, len(r.json()))
-print(r.json()[index]['full_text'])
+def getRandomTweet(username):
+    r = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+username+"&exclude_replies=true&include_rts=false&tweet_mode=extended&since_id=1&count=3200", headers={'Authorization': 'Bearer '+BEARER})
+    index = random.randint(0, len(r.json())-1)
+    print(r.json()[index]['full_text'])
+    return r.json()[index]['full_text']
