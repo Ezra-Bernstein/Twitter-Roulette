036d76b9015b1cff32089ccb8fb0133555251391
Ezra Bernstein <mremdb@gmail.com>
2021-07-18 08:17:09 -0400

    Fixed url
    added /

--- Godot/Global.gd
+++ Godot/Global.gd
@@ -5,7 +5,7 @@ extends Node
 # var a = 2
 # var b = "text"
 var GAME_CODE = ""
-var url = "https://2d856765833e.ngrok.io"
+var url = "https://2d856765833e.ngrok.io/"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
